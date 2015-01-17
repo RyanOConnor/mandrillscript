31b38bf880cd92b43dba65ac7ef3e2ae987515ad
RyanOConnor <RyanMOConnor@lewisu.edu>
2015-01-16 19:02:03 -0600

    Sends email through Mandrill API

---
+++ mandrill.js
@@ -0,0 +1,29 @@
+$("#feedbackbutton").click(function() {
+	var message = $("#messagebox").val();
+
+	$.ajax({
+	  type: "POST",
+	  url: "https://mandrillapp.com/api/1.0/messages/send.json",
+	  data: {
+	    "key": "cVyH6dktBNIpjAev7pvaww",
+	    "message": {
+	      "from_email": "psefeedback@gmail.com",
+	      "to": [
+	          {
+	            "email": "nkasala@psegammazeta.org",
+	            "name": "RECIPIENT NAME (OPTIONAL)",
+	            "type": "to"
+	          },
+	          {
+	            "email": "sirryanoconnor@gmail.com",
+	            "name": "ANOTHER RECIPIENT NAME (OPTIONAL)",
+	            "type": "to"
+	          }
+	        ],
+	      "autotext": "true",
+	      "subject": "PSE Anonymous Feedback",
+	      "html": message
+	    }
+	  }
+	 });
+});
